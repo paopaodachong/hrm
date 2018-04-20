51eb01a6e9e4278a875698dd7994612604541cff
DESKTOP-OPDPISA\陈佳乐 <1026884629@qq.com>
2018-04-20 16:37:28 +0800

    工程微调

---
+++ hrm04/src/main/java/com/cjl/biz/VistorServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cjl.biz;
+
+import com.cjl.dao.VistorMapper;
+import com.cjl.model.Vistor;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by 陈佳乐 on 2018/4/20.
+ */
+@Service
+public class VistorServiceImpl implements VistorServicec {
+    @Resource
+    private VistorMapper vistorMapper;
+    @Override
+    public Vistor login(Vistor vistor) {
+        return vistorMapper.login(vistor);
+    }
+}

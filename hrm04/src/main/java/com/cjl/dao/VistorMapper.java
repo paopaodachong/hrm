824b4e12a59d5cbf8d9cbd169170b131dc448973
DESKTOP-OPDPISA\陈佳乐 <1026884629@qq.com>
2018-04-20 16:08:09 +0800

    工程微调

---
+++ hrm04/src/main/java/com/cjl/dao/VistorMapper.java
@@ -0,0 +1,10 @@
+package com.cjl.dao;
+
+import com.cjl.model.Vistor;
+
+/**
+ * Created by 陈佳乐 on 2018/4/20.
+ */
+public interface VistorMapper {
+    Vistor login(Vistor vistor);
+}
---
+++ hrm04/src/main/java/com/cjl/model/Vistor.java
@@ -0,0 +1,7 @@
+package com.cjl.model;
+
+/**
+ * Created by 陈佳乐 on 2018/4/20.
+ */
+public class Vistor {
+}
